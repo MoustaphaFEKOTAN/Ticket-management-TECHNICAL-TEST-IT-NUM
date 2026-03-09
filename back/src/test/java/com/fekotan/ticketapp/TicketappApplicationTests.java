2b42af79333f13df232b890620c5d3372dccca37
Moustapha Fekotan <moustaphafek@gmail.com>
2026-03-09 01:41:37 +0100

    first commit

---
+++ back/src/test/java/com/fekotan/ticketapp/TicketappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fekotan.ticketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
