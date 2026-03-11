49a57036fa738772c15580967103ac1989a4065c
Moustapha Fekotan <moustaphafek@gmail.com>
2026-03-11 07:47:20 +0100

    fix redirection problems

--- front/app/middleware/auth.global.ts
+++ front/app/middleware/auth.global.ts
@@ -3,11 +3,11 @@ import { useAuthStore } from "~/stores/auth";
 export default defineNuxtRouteMiddleware((to) => {
 
   if (!process.client) return;
-  
+
   const auth = useAuthStore();
   auth.hydrate();
 
-  const publicRoutes = ["/login", "/register"];
+  const publicRoutes = ["/", "/login", "/register"];
 
   if (auth.isAuthenticated && publicRoutes.includes(to.path)) {
     return navigateTo("/tickets");
